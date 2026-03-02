459cc6cdf6484ac2dce0f2976629cf313d45704b
cho45 <cho45@lowreal.net>
2026-03-02 09:27:53 +0900

    update

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://localhost:8001',
     trace: 'on-first-retry',
   },
   expect: {
